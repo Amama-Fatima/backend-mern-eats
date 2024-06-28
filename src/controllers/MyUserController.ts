b74bf967415e6897d53131933a999cedc6119b7e
Amama-Fatima <amamafatima58@gmail.com>
2024-06-28 23:28:03 +0500

    cors

---
+++ src/controllers/MyUserController.ts
@@ -0,0 +1,61 @@
+import { Response, Request } from "express";
+import User from "../models/user";
+
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.json(currentUser.toObject());
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error getting user" });
+  }
+};
+
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+    if (existingUser) {
+      console.log("User already exists");
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    console.log("User created");
+
+    return res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error creating user" });
+  }
+};
+
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+    res.send(user.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
+export default {
+  getCurrentUser,
+  createCurrentUser,
+  updateCurrentUser,
+};
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoutes";
+import restaurantRoute from "./routes/RestaurantRoutes";
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to db"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Create express server
+const app = express();
+//adding middleware to automatically convert req of out api to json
+app.use(express.json());
+//adding middleware to allow cross origin requests
+app.use(
+  cors({
+    origin: "https://frontend-mern-eats-app.onrender.com",
+  })
+);
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health ok!" });
+});
+
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
+});
